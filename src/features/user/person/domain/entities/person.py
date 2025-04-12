4cb31c7254228864abb6000246f5eb82f0635225
sebastiannarvaez23 <narvaezsebas8@gmail.com>
2025-04-12 13:55:42 -0500

    Complementando con nuevos atributos la entidad de personas

--- src/features/user/person/domain/entities/person.py
+++ src/features/user/person/domain/entities/person.py
@@ -11,5 +11,11 @@ class Person(Base):
     __tablename__ = 'persons'
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
-    name = Column(String, index=True)
-    email = Column(String, unique=True, index=True)
+    first_name = Column(String, nullable=False)
+    second_name = Column(String, nullable=False)
+    first_last_name = Column(String, nullable=False)
+    second_last_name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone = Column(String(10), nullable=False)
+    birth_date = Column(String, nullable=False)
+    avatar = Column(String, nullable=True) 
